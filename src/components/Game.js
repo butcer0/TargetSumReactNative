a22f2cf5c37b2c9d9a4a470ba6a25b8736922371
Erik Butcher <butcer0@gmail.com>
2018-05-01 14:25:45 -0400

    Added SelectedNumber[] array and styling selected

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,9 @@ export default class Game extends Component<Props> {
   static propTypes = {
     randomNumberCount: PropTypes.number.isRequired,
   };
+  state = {
+    selectedNumbers: [0,4],
+  };
   randomNumbers = Array
     .from({ length: this.props.randomNumberCount})
     .map(() => 1 + Math.floor(10 * Math.random()));
@@ -15,13 +18,19 @@ export default class Game extends Component<Props> {
       .slice(0, this.props.randomNumberCount - 2)
       .reduce((acc, curr) => acc + curr, 0);
       //TODO: Shuffle the random numbers
+    isNumberSelected = (numberIndex) => {
+      //Erik - 5/1/2018 If number doesn't exist return -1, so false
+      return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+    };
     render() {
       return (
         <View style={styles.container}>
           <Text style={styles.target}>{this.target}</Text>
           <View style={styles.randomContainer}>
             {this.randomNumbers.map((randomNumber, index) =>
-              <RandomNumber key={index} number={randomNumber} />
+              <RandomNumber key={index} number={randomNumber} 
+                isSelected={this.isNumberSelected(index)}
+              />
             )}
           </View>
         </View>
--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
+// TouchableOpacity
+// TouchableHighlight
 
 type Props = {};
 export default class RandomNumber extends Component<Props> {
-  static propTypes = {
-    number: PropTypes.number.isRequired
-  };
-  render() {
-    return (
-      <Text style={styles.random}>{this.props.number}</Text>
-    );
-  }
+    static propTypes = {
+      number: PropTypes.number.isRequired,
+      isSelected: PropTypes.bool.isRequired,
+    };
+    handlePress = () => {
+    //   console.log(this.props.number);
+
+    };
+
+    render() {
+      return (
+      //Erik - 5/1/2018 this.props.isSelected -> only add styles.selected if isSelected is true (basically that the style exists and the prop is true)
+        <TouchableOpacity onPress={this.handlePress}>
+          <Text style={[styles.random, this.props.isSelected && styles.selected]}>
+            {this.props.number}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
 }
 
-const styles = StyleSheet.create({  
+const styles = StyleSheet.create({
   random: {
     backgroundColor: '#999',
     width: 100,
     marginHorizontal: 15,
     marginVertical: 25,
     fontSize: 35,
-    textAlign: 'center'    
+    textAlign: 'center',
+  },
+  selected: {
+    opacity: 0.3,
   },
 });
