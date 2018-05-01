7a95132132f94866aeeddcbdf805fc36d35ada68
Erik Butcher <butcer0@gmail.com>
2018-05-01 15:36:39 -0400

    cached gameStatus and only update as necessary

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,6 +13,7 @@ export default class Game extends Component<Props> {
     selectedIds: [],
     remainingSeconds: this.props.initialSeconds,
   };
+  gameStatus = 'PLAYING';
   randomNumbers = Array
     .from({ length: this.props.randomNumberCount})
     .map(() => 1 + Math.floor(10 * Math.random()));
@@ -51,51 +52,86 @@ export default class Game extends Component<Props> {
         selectedIds: [...prevState.selectedIds, numberIndex],
       }));
     };
+ 
+    //Erik - 5/1/2018 Before next loop, determine if the selectedIds has updated or ran out of time then update cached gameStatus
+    componentWillUpdate(nextProps, nextState) {
+      if(nextState.selectedIds !== this.state.selectedIds 
+        || nextState.remainingSeconds === 0)
+      {
+        //Erik - 5/1/2018 This would be wrong bc it would be calculating on current (previous) state
+        // this.gameStatus = this.calcGameStatus();
+        this.gameStatus = this.calcGameStatus(nextState);
+        if(this.gameStatus != 'PLAYING')
+        {
+          clearInterval(this.intervalId);
+        }
+      } 
+    }
+
   // gameStatus: PLAYING, WON, LOST
-    gameStatus = () => {
-      const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
-        return acc + this.randomNumbers[curr];
-      },0);
-      //Erik - 5/1/2018 Shows on device
-      // console.warn('Current Sum: '+sumSelected);
-      // console.log('Current Sum: '+sumSelected);
-      if(sumSelected > this.target
-        || this.state.remainingSeconds <= 0) {
-        return 'LOST';
-      }
-      if(sumSelected < this.target) {
-        return 'PLAYING';
-      }
-      if(sumSelected == this.target) {
-        return 'WON';
-      }
-    
+  calcGameStatus = (nextState) => {
+    const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+      return acc + this.randomNumbers[curr];
+    },0);
+    //Erik - 5/1/2018 Shows on device
+    // console.warn('Current Sum: '+sumSelected);
+    // console.log('Current Sum: '+sumSelected);
+    if(sumSelected > this.target
+      || nextState.remainingSeconds <= 0) {
+      return 'LOST';
     }
+    if(sumSelected < this.target) {
+      return 'PLAYING';
+    }
+    if(sumSelected == this.target) {
+      return 'WON';
+    }
+  }  
+
+  //Erik - 5/1/2018 This would have calculated previous state when using componentWillUpdate
+  // calcGameStatus = () => {
+  //   const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
+  //     return acc + this.randomNumbers[curr];
+  //   },0);
+  //     //Erik - 5/1/2018 Shows on device
+  //     // console.warn('Current Sum: '+sumSelected);
+  //     // console.log('Current Sum: '+sumSelected);
+  //   if(sumSelected > this.target
+  //       || this.state.remainingSeconds <= 0) {
+  //     return 'LOST';
+  //   }
+  //   if(sumSelected < this.target) {
+  //     return 'PLAYING';
+  //   }
+  //   if(sumSelected == this.target) {
+  //     return 'WON';
+  //   }
+  // }
     
-    render() {
-      const gameStatus = this.gameStatus();
-      return (
-        //Erik - 5/1/2018 Use dynamic string to select the correct class
-        <View style={styles.container}>
-          <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
-            {this.target}
-          </Text>
-          <View style={styles.randomContainer}>
-            {this.randomNumbers.map((randomNumber, index) =>
-              <RandomNumber 
-                key={index}
-                id={index}
-                number={randomNumber}
-                isDisabled={this.isNumberSelected(index) || gameStatus !== 'PLAYING'}
-                onPress={this.selectNumber}
-              />
-            )}
-          </View>
-          <Text>{gameStatus}</Text>
-          <Text>{this.state.remainingSeconds}</Text>
+  render() {
+    // const gameStatus = this.gameStatus;
+    return (
+    //Erik - 5/1/2018 Use dynamic string to select the correct class
+      <View style={styles.container}>
+        <Text style={[styles.target, styles[`STATUS_${this.gameStatus}`]]}>
+          {this.target}
+        </Text>
+        <View style={styles.randomContainer}>
+          {this.randomNumbers.map((randomNumber, index) =>
+            <RandomNumber 
+              key={index}
+              id={index}
+              number={randomNumber}
+              isDisabled={this.isNumberSelected(index) || this.gameStatus !== 'PLAYING'}
+              onPress={this.selectNumber}
+            />
+          )}
         </View>
-      );
-    }
+        <Text>{this.gameStatus}</Text>
+        <Text>{this.state.remainingSeconds}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({  
